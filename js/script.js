e5703bb4af906d28f7fb56afd1e25c96fa136b81
Artur Schulz <artur.schulz123@gmail.com>
2018-05-20 19:30:56 +0200

    RWD3

--- js/script.js
+++ js/script.js
@@ -1,16 +1,61 @@
-     var swiper = new Swiper('.swiper-container', {
-         spaceBetween: 30,
-         centeredSlides: true,
-         autoplay: {
-             delay: 2500,
-             disableOnInteraction: false,
-         },
-         pagination: {
-             el: '.swiper-pagination',
-             clickable: true,
-         },
-         navigation: {
-             nextEl: '.swiper-button-next',
-             prevEl: '.swiper-button-prev',
-         },
-     });
+//karuzela
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+///////////////////////////////////////////////////////////////////////////////////////////////
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
+//Scroll Top
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
+$(function() { $(".scroll").click(function() { $("html,body").animate({ scrollTop: $("#jumbotron").offset().top }, "1000"); return false }) });
